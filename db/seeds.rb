3931a8fc7835bac0729ddbf5bd8ff0106616069e
Yann Klein <yann.klein@me.com>
2022-07-02 09:50:24 +0900

    built data layer of app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Destroy all the restaurants"
+Restaurant.destroy_all
+
+puts "Create 40 restos"
+40.times do 
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    rating: rand(1..5)
+  )
+end
+
+puts "Done!"
