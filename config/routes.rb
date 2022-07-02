25d84269ac27a0276b360607bf40c55f1267fae5
Yann Klein <yann.klein@me.com>
2022-07-02 11:02:29 +0900

    created crud actions and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   # verb 'path', to: 'controller#action', as: 'prefix'
   # See all restaurants
-  root to: 'restaurants#index', as: 'restaurants'
-  # get 'restaurants', to: 'restaurants#index', as: 'restaurants'
-  # See details about one restaurant
-  get 'restaurants/:id', to: 'restaurants#show', as: 'restaurant'
+  # root to: 'restaurants#index', as: 'restaurants'
+  get 'restaurants', to: 'restaurants#index', as: 'restaurants'
   # Create a restaurant
   get 'restaurants/new', to: 'restaurants#new', as: 'new_restaurant'
   post 'restaurants', to: 'restaurants#create'
+  # See details about one restaurant
+  get 'restaurants/:id', to: 'restaurants#show', as: 'restaurant'
   # Update a restaurant
   get 'restaurants/:id/edit', to: 'restaurants#edit', as: 'edit_restaurant'
   patch 'restaurants/:id', to: 'restaurants#update'
