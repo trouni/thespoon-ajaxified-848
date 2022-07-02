2582102b1e1303deb3d394317819c95adc2f6226
Yann Klein <yann.klein@me.com>
2022-07-02 10:13:46 +0900

    created routes and controller skeleton

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # verb 'path', to: 'controller#action', as: 'prefix'
+  # See all restaurants
+  root to: 'restaurants#index', as: 'restaurants'
+  # get 'restaurants', to: 'restaurants#index', as: 'restaurants'
+  # See details about one restaurant
+  get 'restaurants/:id', to: 'restaurants#show', as: 'restaurant'
+  # Create a restaurant
+  get 'restaurants/new', to: 'restaurants#new', as: 'new_restaurant'
+  post 'restaurants', to: 'restaurants#create'
+  # Update a restaurant
+  get 'restaurants/:id/edit', to: 'restaurants#edit', as: 'edit_restaurant'
+  patch 'restaurants/:id', to: 'restaurants#update'
+  # Destroy a restaurant
+  delete 'restaurants/:id', to: 'restaurants#destroy'
+
+  # resources :restaurants
 end
